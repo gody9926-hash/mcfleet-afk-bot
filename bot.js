f20c9cbb0fc4240ea56edd5cddd27062c6a598c7
gody9926-hash <gody9926@gmail.com>
2025-08-02 19:05:09 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+const mineflayer = require('mineflayer');
+
+const bot = mineflayer.createBot({
+  host: 'mcfleet.net',
+  port: 25565,
+  username: 'GOD_GAMERZ_XD',
+  version: '1.21.4',
+  auth: 'offline'
+});
+
+bot.once('spawn', () => {
+  console.log('Bot has spawned!');
+  setInterval(() => {
+    bot.setControlState('jump', true);
+    setTimeout(() => bot.setControlState('jump', false), 500);
+  }, 10000);
+});

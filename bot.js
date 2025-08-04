cc684cdcdbe0026793c260cb25ed0605cfba0baa
gody9926-hash <gody9926@gmail.com>
2025-08-04 14:56:32 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,39 +5,36 @@ function createBot() {
     host: 'mcfleet.net',
     port: 25565,
     username: 'GOD_GAMERZ_XD',
-    auth: 'offline',
-    version: '1.20.1'
+    version: '1.20.1',
+    auth: 'offline'
+  });
+
+  bot.on('login', () => {
+    console.log('Bot has logged in.');
   });
 
   bot.once('spawn', () => {
-  console.log('Bot spawned!');
-  setTimeout(() => {
-    bot.chat('/login GODGAMERZ9998');
+    console.log('Bot spawned!');
+    // Delay to prevent ECONNRESET from spamming too early
     setTimeout(() => {
-      bot.chat('/joinq survival-2');
-    }, 5000);
-  }, 5000);
-});
-
-    setTimeout(() => {
-      bot.chat('/joinq survival-2');
+      bot.chat('/login GODGAMERZ9998');
+      setTimeout(() => {
+        bot.chat('/joinq survival-2');
+      }, 5000);
     }, 5000);
   });
 
-  bot.on('chat', (username, message) => {
-    if (username === bot.username) return;
-    if (message.toLowerCase() === 'hi') {
-      bot.chat('hlo');
+  bot.on('error', (err) => {
+    console.log('Bot error:', err.code || err);
+    if (err.code === 'ECONNRESET') {
+      console.log('Retrying in 10s...');
+      setTimeout(createBot, 10000);
     }
   });
 
   bot.on('end', () => {
     console.log('Bot disconnected, reconnecting in 10s...');
     setTimeout(createBot, 10000);
-  });
-
-  bot.on('error', (err) => {
-    console.log('Bot error:', err);
   });
 }
 
